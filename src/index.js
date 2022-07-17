3943c654ed0c907cf242a0f9fb1ff00d1d367045
danielbatres <danielbatresuser@gmail.com>
2022-07-16 21:45:05 -0600

    Styles in React components

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.createRoot(<App />, document.getElementById("app"));

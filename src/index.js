671abe4e8655a88c7fa917c869aaceeff6a176eb
danielbatres <danielbatresuser@gmail.com>
2022-07-17 11:15:06 -0600

    React Router DOM

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './components/App';
+import App from './routes/App';
 
 const rootElement = document.getElementById("app");
 

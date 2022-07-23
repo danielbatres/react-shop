f23cce256192ec61cdab055dae07924bda22eeb4
danielbatres <danielbatresuser@gmail.com>
2022-07-22 18:04:37 -0600

    Alias and images

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
-import "../styles/Header.scss";
-import menu from "../assets/icons/icon_menu.svg";
-import logo from "../assets/logos/logo_yard_sale.svg"
-import cart from "../assets/icons/icon_shopping_cart.svg";
+import '@styles/Header.scss';
+
+import menu from "@icons/icon_menu.svg";
+import logo from "@logos/logo_yard_sale.svg";
+import cart from "@icons/icon_shopping_cart.svg";
 
 const Header = () => {
     return (
       <nav>
         <img src={menu} alt="menu" className="menu" />
         <div className="navbar-left">
-          <img src={logo} alt="logo" className="logo" />
+          <img src={logo} alt="logo" className="nav-logo" />
           <ul>
             <li>
               <a href="/">All</a>
